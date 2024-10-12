4e0387512714b86db94c232504bbbda0f58f3582
realbrodiwhite <149413096+realbrodiwhite@users.noreply.github.com>
2024-10-12 07:42:13 +0000

    update gitignore

--- royalgames-client/src/features/game-list/GameList.js
+++ royalgames-client/src/features/game-list/GameList.js
@@ -4,35 +4,39 @@ import rockClimberLogo from '../../assets/img/rock-climber-logo.png';
 import egyptianTreasuresLogo from '../../assets/img/egyptian-treasures-logo.png';
 import { useSelector } from 'react-redux';
 
-const GameList = (props) => {
-
+const GameList = () => {
   const loggedIn = useSelector((state) => state.lobby.loggedIn);
+
+  const games = [
+    {
+      logo: egyptianTreasuresLogo,
+      name: 'Egyptian Treasures',
+      path: '/play/egyptian-treasures',
+      alt: 'Egyptian Treasures Slots Game Logo - Royal Games Social Casino',
+      ariaLabel: 'Play Egyptian Treasures Slot Game',
+    },
+    {
+      logo: rockClimberLogo,
+      name: 'Rock Climber',
+      path: '/play/rock-climber',
+      alt: 'Rock Climber Slots Game Logo - Royal Games Social Casino',
+      ariaLabel: 'Play Rock Climber Slot Game',
+    },
+  ];
 
   return (
     <div className="GameList">
-      <div className={`list ${!loggedIn ? 'd-none' : ''}`}>
-        <div className="game" style={{position: 'relative'}}>
-          <img className="logo" src={egyptianTreasuresLogo} alt="Egyptian Treasures Slots Logo - Royal Games Social Casino"/>
-          
-          <span>Egyptian Treasures</span>
-
-          <Link to="play/egyptian-treasures" className="btn-play">
-            Play
-          </Link>
-        </div>
-
-        <div className="game">
-          <img className="logo" src={rockClimberLogo} alt="Rock Climber Slots Logo - Royal Games Social Casino"/>
-          
-          <span>Rock Climber</span>
-
-          <Link to="play/rock-climber" className="btn-play">
-            Play
-          </Link>
-        </div>
+      <div className="list">
+        {games.map((game) => (
+          <div className="game" key={game.name} style={{ position: 'relative' }}>
+            <img className="logo" src={game.logo} alt={game.alt} />
+            <span>{game.name}</span>
+            <Link to={game.path} className="btn-play" aria-label={game.ariaLabel}>
+              Play
+            </Link>
+          </div>
+        ))}
       </div>
     </div>
   );
-}
-
-export default GameList;
+};
